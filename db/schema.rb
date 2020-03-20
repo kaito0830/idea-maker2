c60a271365f004badb6fc647aa03b743462e5e1f
kaito0830 <kaitobad830@outlook.jp>
2020-03-20 11:00:27 +0900

    add user icon

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_015123) do
+ActiveRecord::Schema.define(version: 2020_03_19_101455) do
 
   create_table "ideas", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_03_19_015123) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "image"
   end
 
   add_foreign_key "likes", "ideas"
